0c513432ed68b06f8acc61b3685c64ee426ecf44
yumi-hue <81012232+yumi-hue@users.noreply.github.com>
2021-09-17 17:26:30 +0800

    Update main.py

--- 3219005444/main.py
+++ 3219005444/main.py
@@ -31,3 +31,29 @@ def sim(text1, text2):
     article = dictionary.doc2bow(text1)
     cos = similarity[article][1]
     return cos
+def count():
+    path1 = input("输入论文原文的文件的绝对路径：")
+    path2 = input("输入抄袭版论文的文件的绝对路径：")
+    if not os.path.exists(path1) :
+        print("论文原文文件不存在！")
+        exit()
+    if not os.path.exists(path2):
+        print("抄袭版论文文件不存在！")
+        exit()
+    str1 = file(path1)
+    str2 = file(path2)
+    text1 = filter(str1)
+    text2 = filter(str2)
+    similarity = calc_similarity(text1, text2)   #生成的similarity变量类型为<class 'numpy.float32'>
+    result=round(similarity.item(),2)  #借助similarity.item()转化为<class 'float'>，然后再取小数点后两位
+    return result
+
+
+if __name__ == '__main__':
+    result = count()
+    save_path = input("输入指定文件路径: ")
+    print("文章相似度： %.4f" % result)
+    #将相似度结果写入指定文件
+    f = open(save_path, 'w', encoding="utf-8")
+    f.write("文章相似度： %.4f"%result)
+    f.close()
